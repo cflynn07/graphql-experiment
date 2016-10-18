c3259c75356dd79a268b353fc60859031e6dcaac
Casey Flynn <cflynn.us@gmail.com>
2016-10-18 14:25:25 +0800

    Seed basic graphql logic

--- api/index.js
+++ api/index.js
@@ -1,10 +1,25 @@
 const express = require('express')
+const graphqlHTTP = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const schema = buildSchema(`
+  type Query {
+    hello: String,
+    foo: String
+  }
+`)
+const root = {
+  hello: () => 'Hello World! 2',
+  foo: () => 'Bar'
+}
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true
+}))
 
 app.listen(process.env.PORT, () => {
   console.log('listening on port ' + process.env.PORT)
