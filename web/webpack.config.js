e12599d38ffaf74fc19393956b485a4ff6a28937
Casey Flynn <cflynn.us@gmail.com>
2016-11-01 18:10:35 +0800

    Seed react/relay app directory and docker container for testing

---
+++ web/webpack.config.js
@@ -0,0 +1,27 @@
+const config = {
+  entry: './main.js',
+
+   output: {
+    path:'./',
+    filename: 'index.js',
+   },
+
+   devServer: {
+    inline: true,
+    port: 3001,
+    host: '0.0.0.0'
+   },
+
+   module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  }
+}
+
+module.exports = config
