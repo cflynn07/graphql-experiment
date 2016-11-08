7ec7a25f6bb70cc1aa0d6b04aa631150a9b5ce73
Casey Flynn <cflynn.us@gmail.com>
2016-11-08 16:43:09 +0800

    Add simple fom example

--- web/App.jsx
+++ web/App.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!!!
-         </div>
-      );
-   }
+  constructor () {
+    super()
+    this.state = {
+      data: ''
+    }
+    this.updateState = this.updateState.bind(this)
+  }
+
+  render () {
+    return (
+      <div>
+        <input type = "text" value = {this.state.data} onChange = {this.updateState} />
+        <h4>{this.state.data}</h4>
+      </div>
+    )
+  }
+
+  updateState (e) {
+    this.setState({
+      data: e.target.value
+    })
+  }
 }
 
-export default App;
+class Content extends React.Component {
+  render () {
+    return (
+      <h1>Count is: {this.props.myCount}</h1>
+    )
+  }
+}
+
+export default App
