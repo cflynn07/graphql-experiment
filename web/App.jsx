5718ddcb8b06f7713c5f979abc85b689eaced9a8
Casey Flynn <cflynn.us@gmail.com>
2016-11-12 20:25:43 +0800

    Update with simple react-router example

--- web/App.jsx
+++ web/App.jsx
@@ -1,34 +1,18 @@
 import React from 'react'
 
 class App extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      data: ''
-    }
-    this.updateState = this.updateState.bind(this)
-  }
-
   render () {
     return (
       <div>
-        <input type = "text" value = {this.state.data} onChange = {this.updateState} />
-        <h4>{this.state.data}</h4>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+        <hr />
+
+        {this.props.children}
       </div>
-    )
-  }
-
-  updateState (e) {
-    this.setState({
-      data: e.target.value
-    })
-  }
-}
-
-class Content extends React.Component {
-  render () {
-    return (
-      <h1>Count is: {this.props.myCount}</h1>
     )
   }
 }
--- web/main.js
+++ web/main.js
@@ -1,5 +1,19 @@
+import About from './About.jsx'
+import App from './App.jsx'
+import Contact from './Contact.jsx'
+import Home from './Home.jsx'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App.jsx'
+import { Router, Route, Link, hashHistory, IndexRoute } from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render((
+  <Router history = {hashHistory}>
+    <Route path = "/" component = {App}>
+      <IndexRoute component = {Home}/>
+      <Route path = "home" component = {Home}/>
+      <Route path = "about" component = {About}/>
+      <Route path = "contact" component = {Contact}/>
+    </Route>
+  </Router>
+), document.getElementById('app'))
